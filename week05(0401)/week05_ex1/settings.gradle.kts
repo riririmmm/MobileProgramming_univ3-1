8d8861b35eed7ca022cc3f6a8f1c9969144b2594
riririmmm <mr8774@smail.kongju.ac.kr>
2025-04-17 01:20:48 +0900

    예제 5-1 풂

---
+++ week05(0401)/week05_ex1/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "week05_ex1"
+include(":app")
