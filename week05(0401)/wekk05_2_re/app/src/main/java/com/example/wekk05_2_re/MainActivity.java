98270ac6b78d12222ce5e852e00fe8e44b92c712
riririmmm <mr8774@smail.kongju.ac.kr>
2025-04-18 03:33:35 +0900

    풀어보는중..

--- week05(0401)/wekk05_2_re/app/src/main/java/com/example/wekk05_2_re/MainActivity.java
+++ week05(0401)/wekk05_2_re/app/src/main/java/com/example/wekk05_2_re/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.wekk05_2_re;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,13 +13,14 @@ public class MainActivity extends AppCompatActivity {
     EditText edit1, edit2;
     Button btnAdd, btnSub, btnMul, btnDiv;
     TextView textResult;
-
+    String num1, num2;
+    Float result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
     }
 }
