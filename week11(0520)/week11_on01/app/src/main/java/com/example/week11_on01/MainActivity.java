cdd86fbcffb7f639f89976f30bf79aa33fc86b9d
riririmmm <mr8774@smail.kongju.ac.kr>
2025-05-20 00:02:58 +0900

    week11_on01: 그리드뷰 영화 포스터

---
+++ week11(0520)/week11_on01/app/src/main/java/com/example/week11_on01/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.week11_on01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher_foreground);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        setTitle("그리드뷰 영화 포스터");
+
+        final GridView gv = (GridView) findViewById(R.id.gridView1);
+        MyGridAdapter gAdapter = new MyGridAdapter(this);
+        gv.setAdapter(gAdapter);
+    }
+
+
+    public class MyGridAdapter extends BaseAdapter {
+        Context context;
+
+        Integer[] poseterID = {R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+                R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10,
+                R.drawable.mov11, R.drawable.mov12, R.drawable.mov13, R.drawable.mov14, R.drawable.mov15,
+                R.drawable.mov16, R.drawable.mov17, R.drawable.mov18, R.drawable.mov19, R.drawable.mov20,
+                R.drawable.mov21, R.drawable.mov22, R.drawable.mov23, R.drawable.mov24, R.drawable.mov25
+        };
+        public MyGridAdapter(Context c) {
+            context = c;
+        }
+
+        public int getCount() {
+            return poseterID.length;
+        }
+
+        public Object getItem(int arg0) {
+            return null;
+        }
+
+        public long getItemId(int arg0) {
+            return 0;
+        }
+
+        public View getView (int position, View convertView, ViewGroup parent) {
+            ImageView imageview = new ImageView(context);
+            imageview.setLayoutParams(new ViewGroup.LayoutParams(200, 300));
+            imageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageview.setPadding(5,5,5,5);
+
+            imageview.setImageResource(poseterID[position]);
+
+            final int pos = position;
+
+            imageview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    View dialogView = (View) View.inflate(MainActivity.this, R.layout.dialog, null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    ImageView ivPoster = (ImageView) dialogView.findViewById(R.id.ivPoster);
+                    ivPoster.setImageResource(poseterID[pos]);
+                    dlg.setTitle("큰 포스터");
+                    dlg.setView(dialogView);
+                    dlg.setNegativeButton("닫기", null);
+                    dlg.show();
+                }
+            });
+
+            return imageview;
+        }
+    }
+}
