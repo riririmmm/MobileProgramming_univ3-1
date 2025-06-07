6538e55394ea24b9ba3b58b5f52e1acb4887d358
riririmmm <mr8774@smail.kongju.ac.kr>
2025-06-08 05:49:20 +0900

    week11_on01_1: (응용) 그리드뷰 영화 포스터 완성

--- week11(0520)/week11_01_1/app/src/main/java/com/example/week11_01_1/MainActivity.java
+++ week11(0520)/week11_01_1/app/src/main/java/com/example/week11_01_1/MainActivity.java
@@ -36,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.mov16, R.drawable.mov17, R.drawable.mov18, R.drawable.mov19, R.drawable.mov20,
                 R.drawable.mov21, R.drawable.mov22, R.drawable.mov23, R.drawable.mov24, R.drawable.mov25
         };
+
+        String[] movieName = { "써니", "완득이", "괴물", "라디오 스타", "비열한 거리",
+                "왕의 남자", "아일랜드", "웰컴 투 동막골", "헬보이 골든아미", "Back to the Future",
+                "여인의 향기", "쥬라기 공원", "포레스트 검프", "Groundhog Day", "흑성탈출 진화의 시작",
+                "아름다운 비행", "내 이름은 칸", "해리포터", "마더", "킹콩을 들다",
+                "쿵푸팬더 2", "짱구는 못말려", "아저씨", "아바타", "The Godfather" };
+
         public MyGridAdapter(Context c) {
             context = c;
         }
@@ -69,7 +76,8 @@ public class MainActivity extends AppCompatActivity {
                     AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
                     ImageView ivPoster = (ImageView) dialogView.findViewById(R.id.ivPoster);
                     ivPoster.setImageResource(poseterID[pos]);
-                    dlg.setTitle("큰 포스터");
+                    dlg.setTitle(movieName[pos]);
+//                    dlg.setIcon("아이콘아이디");
                     dlg.setView(dialogView);
                     dlg.setNegativeButton("닫기", null);
                     dlg.show();
