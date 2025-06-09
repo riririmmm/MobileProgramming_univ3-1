53667bf0392fba59afe8cb8b3ac6fdf8935179ab
riririmmm <mr8774@smail.kongju.ac.kr>
2025-06-09 22:44:24 +0900

    week14_on01_1 이전 코드 가져오기

---
+++ week14(0603)/week14_on01_1/app/src/main/java/com/example/week14_on01_1/MusicService.java
@@ -0,0 +1,44 @@
+package com.example.week14_on01_1;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class MusicService extends Service {
+
+    MediaPlayer mediaPlayer;
+
+    //    @Nullable         // 지우라는데 왠진 모르겠네
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        android.util.Log.i("서비스 테스트", "onCreate()");
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        android.util.Log.i("서비스 테스트", "onDestroy()");
+        super.onDestroy();
+
+        mediaPlayer.stop();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        android.util.Log.i("서비스 테스트", "onStartCommand()");
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.song1);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
