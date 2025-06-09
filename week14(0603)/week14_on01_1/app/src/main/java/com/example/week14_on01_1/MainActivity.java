4e75c03187813bff5adcaeecd660bcb8dc21e6e7
riririmmm <mr8774@smail.kongju.ac.kr>
2025-06-09 22:49:50 +0900

    week14_on01: (응용) 화면 종료돼도 계속되는 음악 서비스 완성

--- week14(0603)/week14_on01_1/app/src/main/java/com/example/week14_on01_1/MainActivity.java
+++ week14(0603)/week14_on01_1/app/src/main/java/com/example/week14_on01_1/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.week14_on01_1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,23 +18,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("서비스 테스트 예제");
 
+        // 음악 서비스를 실행/중지하기 위한 인텐트 준비
         intent = new Intent(this, MusicService.class);
+
         btnStart = (Button) findViewById(R.id.btnStart);
         btnStop = (Button) findViewById(R.id.btnStop);
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 서비스 시작
                 startService(intent);
-                android.util.Log.i("서비스 테스트", "startService()");
+
+                // 토스트 메시지 출력
+                Toast.makeText(getApplicationContext(), "음악 서비스 시작됨", Toast.LENGTH_SHORT).show();
+
+                // 액티비티 종료 → 앱은 꺼지지만 음악은 계속 나옴
+                finish();
             }
         });
 
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 서비스 중지
                 stopService(intent);
-                android.util.Log.i("서비스 테스트", "stopService");
+
+                // 토스트 메시지 출력
+                Toast.makeText(getApplicationContext(), "음악 서비스 중지됨", Toast.LENGTH_SHORT).show();
             }
         });
     }
